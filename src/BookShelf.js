8565d8ab87bf7b2d5c310930299654c9c10a355f
Samuel Fialho <samuel@superlativo.pt>
2017-12-17 12:08:42 +0000

    added a new component: Book.js

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,39 +1,11 @@
 import React, { Component } from 'react'
-import sortBy from 'sort-by'
+import Book from './Book'
 
 class BookShelf extends Component {
   render() {
-    this.props.booksFiltered.sort(sortBy('title'))
     return(
       <div className="bookshelf-books">
-        <ol className="books-grid">
-          {this.props.booksFiltered.map((book) =>
-            <li key={book.id}>
-              <div className="book">
-                <div className="book-top">
-                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail && book.imageLinks.smallThumbnail})` }}></div>
-                  <div className="book-shelf-changer">
-                    <select value={book.shelf} onChange={(e) => this.props.onChangeShelf(book, e.target.value)}>
-                      <option value="none" disabled>Move to...</option>
-                      <option value="currentlyReading">Currently Reading</option>
-                      <option value="wantToRead">Want to Read</option>
-                      <option value="read">Read</option>
-                      <option value="none">None</option>
-                    </select>
-                  </div>
-                </div>
-                 <div className="book-title">{book.title}</div>
-                <div className="book-authors">
-                  {book.authors.map((author) =>
-                    <div key={author}>
-                      <span>{author}</span>
-                    </div>
-                  )}
-                </div>
-              </div>
-            </li>
-          )}
-        </ol>
+        <Book booksFiltered={this.props.booksFiltered} onChangeShelf={this.props.onChangeShelf} />
       </div>
     )
   }
